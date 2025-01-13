faff80d858a2e3bfd952cc934bb3693517f9b8b7
Abdelkhalek Ashraf <abdelkhalek.bedear01@eng-st.cu.edu.eg>
2025-01-13 19:22:46 +0200

    speed up emails retrive

--- actions/getemails.ts
+++ actions/getemails.ts
@@ -32,7 +32,10 @@ async function decodeBase64(base64String: string): Promise<string> {
   return buffer.toString("utf-8");
 }
 
-async function getMessages(access_token: string): Promise<MessageDto[]> {
+async function getMessages(
+  access_token: string,
+  category: string,
+): Promise<MessageDto[]> {
   try {
     // Initialize the OAuth2 client with the access token
     const oauth2Client = new google.auth.OAuth2();
@@ -46,82 +49,97 @@ async function getMessages(access_token: string): Promise<MessageDto[]> {
     // List the user's messages (inbox, max 50 results)
     const listResponse = await gmail.users.messages.list({
       userId: "me",
+      labelIds: category ? [category] : undefined,
       maxResults: 10,
     });
 
-    const messages: MessageDto[] = [];
-    for (const message of listResponse.data.messages || []) {
-      const messageDetailResponse = await gmail.users.messages.get({
-        userId: "me",
-        id: message.id!,
-        format: "full", // Get the full content
-      });
+    // const messages: MessageDto[] = [];
+    // for (const message of listResponse.data.messages || []) {
+    if (listResponse.data.messages) {
+      const detailedMessages: MessageDto[] = await Promise.all(
+        listResponse.data.messages.map(async (message) => {
+          const messageDetailResponse = await gmail.users.messages.get({
+            userId: "me",
+            id: message.id!,
+            format: "full", // Get the full content
+          });
 
-      const messageDto: MessageDto = {
-        id: message.id!,
-        threadId: message.threadId!,
-        snippet: messageDetailResponse.data.snippet!,
-        labelIds: messageDetailResponse.data.labelIds!,
-        payload: (messageDetailResponse.data.payload as MessagePayload) || null,
-      };
+          const messageDto: MessageDto = {
+            id: message.id!,
+            threadId: message.threadId!,
+            snippet: messageDetailResponse.data.snippet!,
+            labelIds: messageDetailResponse.data.labelIds!,
+            payload:
+              (messageDetailResponse.data.payload as MessagePayload) || null,
+          };
 
-      // Extract headers like "Subject", "From", and "Date"
-      if (messageDto.payload?.headers) {
-        const headers = messageDto.payload.headers;
-        messageDto.subject = headers.find((h) => h.name === "Subject")?.value;
-        messageDto.from = headers.find((h) => h.name === "From")?.value;
-        messageDto.date = headers.find((h) => h.name === "Date")?.value;
-      }
+          // Extract headers like "Subject", "From", and "Date"
+          if (messageDto.payload?.headers) {
+            const headers = messageDto.payload.headers;
+            messageDto.subject = headers.find(
+              (h) => h.name === "Subject",
+            )?.value;
+            messageDto.from = headers.find((h) => h.name === "From")?.value;
+            messageDto.date = headers.find((h) => h.name === "Date")?.value;
+          }
 
-      // Handle different MIME types and decode the body if necessary
-      if (messageDto.payload) {
-        const { mimeType, parts } = messageDto.payload;
+          // Handle different MIME types and decode the body if necessary
+          if (messageDto.payload) {
+            const { mimeType, parts } = messageDto.payload;
 
-        if (mimeType === "text/html" && messageDto.payload.body.data) {
-          const decodedBody = await decodeBase64(messageDto.payload.body.data);
-          messageDto.decodedBody = decodedBody;
-        }
+            if (mimeType === "text/html" && messageDto.payload.body.data) {
+              const decodedBody = await decodeBase64(
+                messageDto.payload.body.data,
+              );
+              messageDto.decodedBody = decodedBody;
+            }
 
-        if (mimeType === "multipart/alternative") {
-          for (const part of parts || []) {
-            if (part.body?.data) {
-              const decodedBody = await decodeBase64(part.body.data);
-              part.body.data = decodedBody;
+            if (mimeType === "multipart/alternative") {
+              for (const part of parts || []) {
+                if (part.body?.data) {
+                  const decodedBody = await decodeBase64(part.body.data);
+                  part.body.data = decodedBody;
+                  messageDto.decodedBody = decodedBody;
+                }
+              }
+            }
+
+            if (
+              mimeType === "multipart/mixed" &&
+              parts &&
+              parts[0]?.mimeType === "multipart/alternative"
+            ) {
+              for (const part of parts[0]?.parts || []) {
+                if (part.body?.data) {
+                  const decodedBody = await decodeBase64(part.body.data);
+                  part.body.data = decodedBody;
+                  messageDto.decodedBody = decodedBody;
+                }
+              }
+            }
+
+            if (
+              mimeType === "multipart/mixed" &&
+              parts &&
+              parts[0]?.mimeType === "text/html"
+            ) {
+              const decodedBody = await decodeBase64(
+                parts[0]?.body?.data || "",
+              );
+              parts[0].body.data = decodedBody;
               messageDto.decodedBody = decodedBody;
             }
           }
-        }
+          return messageDto;
+        }),
+      );
 
-        if (
-          mimeType === "multipart/mixed" &&
-          parts &&
-          parts[0]?.mimeType === "multipart/alternative"
-        ) {
-          for (const part of parts[0]?.parts || []) {
-            if (part.body?.data) {
-              const decodedBody = await decodeBase64(part.body.data);
-              part.body.data = decodedBody;
-              messageDto.decodedBody = decodedBody;
-            }
-          }
-        }
-
-        if (
-          mimeType === "multipart/mixed" &&
-          parts &&
-          parts[0]?.mimeType === "text/html"
-        ) {
-          const decodedBody = await decodeBase64(parts[0]?.body?.data || "");
-          parts[0].body.data = decodedBody;
-          messageDto.decodedBody = decodedBody;
-        }
-      }
-
-      messages.push(messageDto);
-      // Path to the JSON file
+      return detailedMessages;
     }
+    throw new Error("Failed to fetch messages");
 
-    return messages;
+    // messages.push(messageDto);
+    // Path to the JSON file
   } catch (error) {
     console.error("Error fetching messages:", error);
     throw new Error("Failed to fetch messages");
